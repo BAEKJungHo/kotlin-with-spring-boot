546c5c7d7fa458cc180b6ae606bf2cacdb570f58
BAEKJungHo <designjava@naver.com>
2022-02-24 17:29:43 +0900

    Create Projects

---
+++ src/src/main/kotlin/me/baek/study/KotlinprojectApplication.kt
@@ -0,0 +1,11 @@
+package me.baek.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinprojectApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinprojectApplication>(*args)
+}
---
+++ src/src/test/kotlin/me/baek/study/KotlinprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.baek.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinprojectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
