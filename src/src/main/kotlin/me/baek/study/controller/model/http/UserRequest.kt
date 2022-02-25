3262b005e5175701c7cb7dee0a8552edb8703f46
BAEKJungHo <designjava@naver.com>
2022-02-25 19:46:56 +0900

    [feature] JSR-380 Bean Validation

--- src/src/main/kotlin/me/baek/study/controller/model/http/UserRequest.kt
+++ src/src/main/kotlin/me/baek/study/controller/model/http/UserRequest.kt
@@ -1,9 +1,46 @@
 package me.baek.study.controller.model.http
 
+import jdk.jshell.spi.ExecutionControlProvider
+import java.lang.Exception
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.validation.constraints.AssertTrue
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.PositiveOrZero
+import javax.validation.constraints.Size
+
 data class UserRequest(
-    var name:String?=null,
-    var age:Int?=null,
-    var email:String?=null,
-    var address:String?=null,
-    var phoneNumber:String?=null
-)
+        @field:NotEmpty
+        @field:Size(min = 2, max = 8, message = "은 2 ~ 8 자리 사이어야 합니다.")
+        var name:String?=null,
+
+        @field:PositiveOrZero // age >= 0
+        var age:Int?=null,
+
+        @field:Email
+        var email:String?=null,
+
+        @field:NotBlank
+        var address:String?=null,
+
+        @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$")
+        var phoneNumber:String?=null,
+
+        var createdAt:String?=null // yyyy-MM-dd HH:mm:ss
+) {
+    // @AssertTrue 를 붙이면 Validation 이 일어날 때, 해당 어노테이션이 붙은 메서드를 참고하여 실행시킨다.
+    // @AssertTrue : 해당 메서드의 결과가 true 인지 검증
+    // @AssertFalse : 해당 메서드의 결과가 false 인지 검증
+    @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss 여야 합니다.")
+    private fun isValidCreatedAt(): Boolean {
+        return try {
+            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
--- src/src/main/kotlin/me/baek/study/controller/put/PutApiController.kt
+++ src/src/main/kotlin/me/baek/study/controller/put/PutApiController.kt
@@ -3,11 +3,15 @@ package me.baek.study.controller.put
 import me.baek.study.controller.model.http.Result
 import me.baek.study.controller.model.http.UserRequest
 import me.baek.study.controller.model.http.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RequestMapping("/api")
 @RestController
@@ -24,9 +28,24 @@ class PutApiController {
     }
 
     @PutMapping("/put-mapping/object")
-    fun putMappingObject(@RequestBody userRequest : UserRequest): UserResponse {
+    fun putMappingObject(
+            @Valid @RequestBody userRequest : UserRequest,
+            bindingResult: BindingResult
+    ): ResponseEntity<String> {
+        if(bindingResult.hasErrors()) {
+            // 500 Error
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.field).append(message).append("\n")
+            }
+            return ResponseEntity.badRequest().body(msg.toString())
+        }
+        return ResponseEntity.ok("OK")
+        
         // apply pattern 사용 -> 자기 자신 리턴
-
+/*
         // 1. UserResponse
         return UserResponse().apply {
             // 2. result
@@ -60,5 +79,6 @@ class PutApiController {
 
             this.user = userList
         }
+ */
     }
 }
